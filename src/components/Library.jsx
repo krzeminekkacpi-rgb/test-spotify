51f8be56935592a0ffe1be5c15ec4ab0e29b8068
Kacper <kacperkrzeminski3@wp.pl>
2026-05-05 20:40:04 +0200

    Finish the library section css

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -5,7 +5,12 @@ export function Library() {
         <div className="main-Library">
             <div className='Naglowek'>
             <h3 className='Naglowek-text'>Biblioteka</h3>
-            <button className='Utworz'>Utwórz</button>
+            <button className='Utworz' title='Utwórz playliste, folder lub Jam'>➕ Utwórz</button>
+            </div>
+            <div className='Utworz-playliste'>
+                <span className='upp'>Utwórz swoją pierwszą playlistę</span>
+                <span className='down'>To proste, pomożemy Ci</span>
+                <button className='Utworz-playliste-button'>Utwórz playlistę</button>
             </div>
         </div>
     )
